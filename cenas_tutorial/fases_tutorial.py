3ec1ae761520bc09e560c32472324aaf8381d9c2
Vica <vicalou30@gmail.com>
2026-06-29 00:13:47 -0300

    Decks do tutorial

--- cenas_tutorial/fases_tutorial.py
+++ cenas_tutorial/fases_tutorial.py
@@ -1,67 +1,31 @@
 fases_do_tutorial = {
-    "combate1":{
+    "luta_1_tutorial": {
         "script_inimigo": {
-            1:[
-                {"acao": "jogar_carta", "carta": {"nome": "acaua", "vida": 3, "dano": 2}, "slot": 1}
-            ],
-            2: [{}],
-            3: [{"acao": "jogar_carta", "carta": {"nome": "comadre", "vida": 1, "dano": 1}, "slot": 2}]
-        }
-    },
-    
-    "luta_2_mapa_1":{
-        "script_inimigo": {
-            1:[
-                {"acao": "jogar_carta", "carta": {"nome": "mula", "vida": 4, "dano": 3}, "slot": 0}
-            ],
-            2: [],
-            3: [
-                {"acao": "jogar_carta", "carta": {"nome": "comadre", "vida": 1, "dano": 1}, "slot": 2}
-            ]
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Curupira", "vida": 2, "dano": 3}, "slot": 1}],
+            2: [{"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2}, "slot": 3}]
         }
     },
 
-    "luta_3_mapa_1":{
-            "script_inimigo": {
-                1:[
-                    {"acao": "jogar_carta", "carta": {"nome": "Timbu", "vida": 2, "dano": 2}, "slot": 1},
-                    {"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2}, "slot": 3}
-                ],
-                2: [
-                     {"acao": "jogar_carta", "carta": {"nome": "Timbu", "vida": 2, "dano": 2}, "slot": 0},
-                ]
-            }
-        },
-
-    "boss_1": {
-        "nome": "caboclo de lança",
-        "mensagem_inicio": "Tu veio de muito longe né menino, agora bora dançar !",
-        "obstaculos_iniciais": [
-            {"slot": 2, "nome": "Cacto", "vida": 5, "dano": 0, "valor_sacrificio": 0}
-        ],
+    "luta_2_tutorial": {
         "script_inimigo": {
-            1: [
-                {"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 3, "dano": 1}, "slot": 0},
-                {"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2}, "slot": 2}
-            ],
-            2: [
-                {"acao": "jogar_carta", "carta": {"nome": "Cobra Coral", "vida": 2, "dano": 2}, "slot": 3}
-            ],
-            3: [
-                {"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2}, "slot": 0}
-            ],
-            4: [
-                 {"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 3, "dano": 1}, "slot": 0}
-            ],
-            5: [
-                 {"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2}, "slot": 1}
-            ],
-            6: [
-                {"acao": "jogar_carta", "carta": {"nome": "Cobra Coral", "vida": 2, "dano": 2}, "slot": 2}
-            ],
-            7: [
-                {"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2}, "slot": 3}
-            ]
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2}, "slot": 2},
+                {"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2}, "slot": 1}],
+            2: [{"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 3, "dano": 1}, "slot": 1}]
+        }
+    },
+
+    "luta_3_tutorial": {
+        "script_inimigo": {
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 0},
+                {"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2}, "slot": 2}],
+            2: [{"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 1},
+                {"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 3}],
+            3: [{"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 2},
+                {"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 0}],
+            4: [{"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 1},
+                {"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 3}],
+            5: [{"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 2},
+                {"acao": "jogar_carta", "carta": {"nome": "Laursa", "vida": 6, "dano": 4}, "slot": 0}]
         }
     }
 }
