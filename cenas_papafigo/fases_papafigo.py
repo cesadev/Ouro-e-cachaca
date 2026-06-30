ed8b8a76ec24c93ee9d62c5b223bf8418307b223
Vica <vicalou30@gmail.com>
2026-06-30 17:41:32 -0300

    Decks do mapa do papafigo

--- cenas_papafigo/fases_papafigo.py
+++ cenas_papafigo/fases_papafigo.py
@@ -1,21 +1,45 @@
 fases_do_jogo = {
-    "boss_1": {
-        "nome": "o lenhador brabo",
-        "mensagem_inicio": "Sua primeira batalha! Posicione suas cartas para sobreviver.",
-        "obstaculos_iniciais": [
-            {"slot": 2, "nome": "Cacto", "vida": 5, "dano": 0, "valor_sacrificio": 0}
-        ],
+    "luta_1_mapa_2": {
         "script_inimigo": {
-            1: [
-                {"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 3, "dano": 1}, "slot": 0}
-            ],
-            2: [
-                {"acao": "jogar_carta", "carta": {"nome": "Timbu", "vida": 2, "dano": 2}, "slot": 3}
-            ],
-            3: [],
-            4: [
-                {"acao": "ataque_especial", "nome": "Puxão master das trevas", "dano_direto": 1}
-            ]
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Caboclo D’água", "vida": 1, "dano": 1, "selos":["mergulhador"]}, "slot": 0},
+                {"acao": "jogar_carta", "carta": {"nome": "Caboclo D’água", "vida": 1, "dano": 1, "selos":["mergulhador"]}, "slot": 1},
+                {"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 2, "dano": 1, "selos":[]}, "slot": 2}],
+            2: [{"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2, "selos":["voar"]}, "slot": 3}],
+            3: [{"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2, "selos":[]}, "slot": 2}],
+            4: [{"acao": "jogar_carta", "carta": {"nome": "Caboclo D’água", "vida": 1, "dano": 1, "selos":["mergulhador"]}, "slot": 2}],
+            5: [{}],
+            6: [{"acao": "jogar_carta", "carta": {"nome": "Acauã", "vida": 3, "dano": 2, "selos":["voar"]}, "slot": 3}]
+        }
+    },
+
+    "luta_2_mapa_2": {
+        "script_inimigo": {
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Capelobo", "vida": 2, "dano": 1, "selos":[]}, "slot": 1}],
+            2: [{"acao": "jogar_carta", "carta": {"nome": "Boitatá", "vida": 1, "dano": 2, "selos":[]}, "slot": 1},
+                {"acao": "jogar_carta", "carta": {"nome": "Cobra Coral", "vida": 2, "dano": 2, "selos":["mortal"]}, "slot": 2}],
+            3: [{"acao": "jogar_carta", "carta": {"nome": "Curupira", "vida": 2, "dano": 3, "selos":[]}, "slot": 0}],
+            4: [{"acao": "jogar_carta", "carta": {"nome": "Caboclo D’água", "vida": 1, "dano": 1, "selos":["mergulhador"]}, "slot": 3}],
+            5: [{}],
+            6: [{"acao": "jogar_carta", "carta": {"nome": "Mula Sem-Cabeça", "vida": 4, "dano": 3, "selos":[]}, "slot": 0}]
+        }
+    },
+
+     "boss_2": {
+        "nome": "papa-figo",
+        "mensagem_inicio": "Criança mal criada... Chegou a sua hora de ir dormir...",
+        "obstaculos_iniciais": [{"slot": 1, "nome": "Cacto", "vida": 5, "dano": 0, "valor_sacrificio": 0}],
+        "script_inimigo": {
+            1: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 0}],
+            2: [{}],
+            3: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 1}],
+            4: [{}],
+            5: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 2}],
+            6: [{}],
+            7: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 3}],
+            8: [{}],
+            9: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 0}],
+            10: [{}],
+            11: [{"acao": "jogar_carta", "carta": {"nome": "Cuca", "vida": 2, "dano": 2, "selos":["escudo"]}, "slot": 1}]
         }
     }
 }
